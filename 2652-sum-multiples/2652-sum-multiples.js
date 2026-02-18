d2c66de3e6d3f4320d06f9e501523f9563fc7982
christin-johny <christinjohnymankuthel@gmail.com>
2026-02-18 17:37:22 +0530

    Time: 1 ms (91.42%), Space: 54.6 MB (96.7%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.js
+++ 2652-sum-multiples/2652-sum-multiples.js
@@ -1,13 +1,10 @@
-/**
- * @param {number} n
- * @return {number}
- */
+
 var sumOfMultiples = function(n) {
-    let sum=0
-    for(let i=1;i<=n;i++){
-        if(i%3==0 ||i%5==0 ||i%7==0){
-            sum+=i
-        }
+   let sum=0;
+   for(let i=0;i<=n;i++){
+    if(i%3===0 || i%5===0 || i%7===0){
+        sum += i;
     }
-    return sum
+   }
+   return sum;
 };
