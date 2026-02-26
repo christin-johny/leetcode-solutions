c6b6f046e6f6a87f7e5980b2f7829fda1d38360a
christin-johny <christinjohnymankuthel@gmail.com>
2026-02-27 02:37:01 +0530

    Time: 62 ms (59.44%), Space: 129.3 MB (25.49%) - LeetHub

---
+++ 0077-combinations/0077-combinations.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    const result = [];
+
+    function backtrack(start, path) {
+        if (path.length === k) {
+            result.push([...path]);
+            return;
+        }
+
+        for (let i = start; i <= n; i++) {
+            path.push(i);
+            backtrack(i + 1, path);
+            path.pop(); // backtrack
+        }
+    }
+
+    backtrack(1, []);
+    return result;
+};
