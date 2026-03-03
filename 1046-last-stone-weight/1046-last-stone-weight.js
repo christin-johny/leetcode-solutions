aa887a9929da685a0f2dfb68fcf1a2a94c4a8e78
christin-johny <christinjohnymankuthel@gmail.com>
2026-03-04 00:50:32 +0530

    Time: 1 ms (90.19%), Space: 55 MB (81.34%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    while (stones.length > 1) { 
+        stones.sort((a, b) => b - a);
+
+        const y = stones.shift();
+        const x = stones.shift();  
+
+        if (y !== x) {
+            stones.push(y - x);
+        }
+    }
+
+    return stones.length === 1 ? stones[0] : 0;
+};
